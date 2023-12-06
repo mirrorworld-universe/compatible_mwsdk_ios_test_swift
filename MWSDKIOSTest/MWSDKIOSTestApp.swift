039393f4bec21e20cb7a42c8cb8a5b93df38b2aa
Squall <2573040560@qq.com>
2023-12-06 23:30:46 +0800

    Initial Commit

---
+++ MWSDKIOSTest/MWSDKIOSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MWSDKIOSTestApp.swift
+//  MWSDKIOSTest
+//
+//  Created by squall on 2023/12/6.
+//
+
+import SwiftUI
+
+@main
+struct MWSDKIOSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
